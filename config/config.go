955fa42894b7a813b77cae71dbbf464df283461a
Sara-Savic-01 <sara.savic@elfak.rs>
2024-08-15 06:10:50 -0400

    Globalize config variable for auth token handling

--- config/config.go
+++ config/config.go
@@ -18,15 +18,18 @@ type Config struct{
 	DB DBConfig `json:"db"`
 	AuthToken string `json:"auth_token"`
 }
+
+var AppConfig Config
+
 func LoadConfig(filename string) (*Config, error){
 	file, err:=os.Open(filename)
 	if err!=nil{
 		return nil, err
 	}
 	defer file.Close()
-	var config Config
+	
 	decoder:=json.NewDecoder(file)
-	err=decoder.Decode(&config)
+	err=decoder.Decode(&AppConfig)
 	if err!=nil{
 		return nil,err
 	}
