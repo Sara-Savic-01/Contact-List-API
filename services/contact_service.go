42d7ab4bb74ddfa331ecb90cdc4f3a3048024eaf
Sara-Savic-01 <sara.savic@elfak.rs>
2024-08-16 12:43:58 -0400

    Update services

--- services/contact_service.go
+++ services/contact_service.go
@@ -3,6 +3,8 @@ import(
 	"github.com/google/uuid"
 	"contact-list-api-1/models"
 	"contact-list-api-1/repositories"
+	"errors"
+	"regexp"
 )
 type ContactService interface{
 	GetAllContacts() ([]models.Contact, error)
@@ -21,18 +23,57 @@ type contactService struct{
 func NewContactService(repo repositories.ContactRepository) ContactService{
 	return &contactService{repo:repo}
 }
-func (s *contactService) GetAllContacts() ([]models.Contacts, error){
+func (s *contactService) GetAllContacts() ([]models.Contact, error){
 	return s.repo.GetAll()
 }
 func (s *contactService) GetContactByUUID(uuid uuid.UUID) (*models.Contact, error){
+		
 	return s.repo.GetByUUID(uuid)
 }
 func (s *contactService) CreateContact(contact models.Contact) error{
-	return s.rep.Create(contact)
+	
+	if contact.Email!=""&&!isValidEmail(contact.Email){
+		return errors.New("Invalid email format")
+	}	
+	if contact.Mobile!=""&&!isValidMobile(contact.Mobile){
+		return errors.New("Invalid mobile format")
+	}
+	
+	if contact.ListID==0{
+		return errors.New("Contact must belong to a list")
+	}
+	return s.repo.Create(contact)
 }
 func (s *contactService) UpdateContact(contact models.Contact) error{
+	
+	if contact.Email!=""&&!isValidEmail(contact.Email){
+		return errors.New("Invalid email format")
+	}	
+	if contact.Mobile!=""&&!isValidMobile(contact.Mobile){
+		return errors.New("Invalid mobile format")
+	}
+	
+	if contact.ListID==0{
+		return errors.New("Contact must belong to a list")
+	}
+	existingContact, err:=s.repo.GetByUUID(contact.UUID)
+	if err!=nil || existingContact==nil{
+		return errors.New("Contact not found")
+	}
 	return s.repo.Update(contact)
 }
 func (s *contactService) DeleteContact(uuid uuid.UUID) error{
+	
+	
+	
 	return s.repo.Delete(uuid)
+}
+
+func isValidEmail(email string) bool{
+	re:=regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`)
+	return re.MatchString(email)
+}
+func isValidMobile(mobile string) bool{
+	re:=regexp.MustCompile(`^\+[1-9]\d{1,14}$`)
+	return re.MatchString(mobile)
 }
--- services/list_service.go
+++ services/list_service.go
@@ -3,6 +3,7 @@ import(
 	"github.com/google/uuid"
 	"contact-list-api-1/models"
 	"contact-list-api-1/repositories"
+	"errors"
 )
 type ListService interface{
 	GetAllLists() ([]models.List, error)
@@ -23,14 +24,33 @@ func (s *listService) GetAllLists() ([]models.List, error){
 	return s.repo.GetAll()
 }
 func (s *listService) GetListByUUID(uuid uuid.UUID) (*models.List, error){
+		
 	return s.repo.GetByUUID(uuid)
 }
 func (s *listService) CreateList(list models.List) error{
+	if list.Name==""{
+		return errors.New("Name cannot be empty")
+	}       
+	
+        existingList, err:=s.repo.GetByUUID(list.UUID)
+	if err==nil && existingList!=nil{
+		return errors.New("List with this UUID already exists")
+	}	
 	return s.repo.Create(list)
 }
 func (s *listService) UpdateList(list models.List) error{
+	if list.Name==""{
+		return errors.New("Name cannot be empty")
+	}      
+	
+        existingList, err:=s.repo.GetByUUID(list.UUID)
+	if err==nil && existingList!=nil{
+		return errors.New("List with this UUID already exists")
+	}	
+		
 	return s.repo.Update(list)
 }
 func (s *listService) DeleteList(uuid uuid.UUID) error{
+		
 	return s.repo.Delete(uuid)
 }
