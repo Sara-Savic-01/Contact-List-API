612e4c90de6f6856b47e809c037f7aa42f70aae9
Sara-Savic-01 <sara.savic@elfak.rs>
2024-08-23 09:02:48 -0400

    Add authentication middleware and configure API routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"contact-list-api-1/config"
 	"contact-list-api-1/models"
+	"contact-list-api-1/repositories"
+	"contact-list-api-1/services"
+	"contact-list-api-1/handlers"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
 	"fmt"
+	"net/http"
+	"contact-list-api-1/middlewares"
+	
 )
 
 func main() {
@@ -26,4 +32,30 @@ func main() {
 	}
 
 	log.Println("Database connected succesfully")
+
+	listRepo:=repositories.NewListRepository(db)
+	contactRepo:=repositories.NewContactRepository(db)
+
+	listService:=services.NewListService(listRepo)
+	contactService:=services.NewContactService(contactRepo)
+
+	listHandler:=handlers.NewListHandler(listService)
+	contactHandler:=handlers.NewContactHandler(contactService)
+	
+	http.Handle("/lists", middleware.AuthMiddleware(cfg.AuthToken, http.HandlerFunc(listHandler.GetAllLists)))
+	http.Handle("/lists/get/", middleware.AuthMiddleware(cfg.AuthToken, http.HandlerFunc(listHandler.GetListByUUID)))
+	http.Handle("/lists/create", middleware.AuthMiddleware(cfg.AuthToken, http.HandlerFunc(listHandler.CreateList)))
+	http.Handle("/lists/update", middleware.AuthMiddleware(cfg.AuthToken, http.HandlerFunc(listHandler.UpdateList)))
+	http.Handle("/lists/delete/", middleware.AuthMiddleware(cfg.AuthToken, http.HandlerFunc(listHandler.DeleteList)))	
+	
+	http.Handle("/contacts", middleware.AuthMiddleware(cfg.AuthToken, http.HandlerFunc(contactHandler.GetAllContacts)))	
+	http.Handle("/contacts/get/", middleware.AuthMiddleware(cfg.AuthToken, http.HandlerFunc(contactHandler.GetContactByUUID)))	
+	http.Handle("/contacts/create", middleware.AuthMiddleware(cfg.AuthToken, http.HandlerFunc(contactHandler.CreateContact)))
+	http.Handle("/contacts/update", middleware.AuthMiddleware(cfg.AuthToken, http.HandlerFunc(contactHandler.UpdateContact)))
+	http.Handle("/contacts/delete/", middleware.AuthMiddleware(cfg.AuthToken, http.HandlerFunc(contactHandler.DeleteContact)))	
+	
+	log.Println("Starting server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+	
+
