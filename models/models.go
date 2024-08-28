b537e85a9278e9743c5c1a67277d7e940c1f0463
Sara-Savic-01 <sara.savic@elfak.rs>
2024-08-28 15:12:17 -0400

    Moddified list model

--- models/models.go
+++ models/models.go
@@ -8,14 +8,14 @@ type List struct{
 	ID uint `gorm:"primaryKey;autoIncrement"`
 	UUID uuid.UUID `gorm:"type:char(36); not null;uniqueIndex" json:"uuid":` 
 	Name string `gorm:"type:varchar(255);not null" json:"name"`
-	Contacts []Contact `gorm:"foreignKey:ListID" json:"contacts"`
+	
 }
 
 type Contact struct{
 	ID uint `gorm:"primaryKey;autoIncrement"`
 	UUID uuid.UUID `gorm:"type:char(36); not null;uniqueIndex" json:"uuid:` 
 	FirstName string `gorm:"type:varchar(255);not null" json:"first_name"`
-	LastName string `gorm:"type:varchar(255);not null" json:"lastname"`
+	LastName string `gorm:"type:varchar(255);not null" json:"last_name"`
 	Mobile string `gorm:"type:varchar(20);not null" json:"mobile"`
 	Email string `gorm:"type:varchar(255); not null ; uniqueIndex" json:"email"`
 	CountryCode string `gorm:"type:varchar(3);not null" json:"country_code"`
