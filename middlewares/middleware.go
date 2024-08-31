5755ed1427b4f65d1483545847bd79949aec859f
Sara-Savic-01 <sara.savic@elfak.rs>
2024-08-31 15:38:13 -0400

    Added Unauthorized check in middleware and corresponding test

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -7,6 +7,10 @@ import (
 func AuthMiddleware(token string, next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+            http.Error(w, "Unauthorized", http.StatusUnauthorized)
+            return
+        }
         if authHeader != "Bearer "+token {
             http.Error(w, "Forbidden", http.StatusForbidden)
             return
--- middlewares/middleware_test.go
+++ middlewares/middleware_test.go
@@ -48,5 +48,26 @@ func TestAuthMiddleware(t *testing.T){
 			t.Errorf("Expected body 'OK', got '%s'", body)
 		}
 	})
+	t.Run("Missing Authorization header", func(t *testing.T) {
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		    w.WriteHeader(http.StatusOK)
+		    w.Write([]byte("OK"))
+		})
+		token := "Axf2FVAusahoXmKMLZih7LrhBwmYLVmyLDiMoYizPGReJTKEaseAb12oGYvbLleS"
+		middleware := AuthMiddleware(token, handler)
+		req, err := http.NewRequest("GET", "/lists", nil)
+		if err != nil {
+		    t.Fatalf("Could not create request: %v", err)
+		}
+		
+		rr := httptest.NewRecorder()
+		middleware.ServeHTTP(rr, req)
+		if status := rr.Code; status != http.StatusUnauthorized {
+		    t.Errorf("Expected status code %d, got %d", http.StatusUnauthorized, status)
+		}
+		if body := rr.Body.String(); body != "Unauthorized\n" {
+		    t.Errorf("Expected body 'Unauthorized', got '%s'", body)
+		}
+    	})
 }
 	
